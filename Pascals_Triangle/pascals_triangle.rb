6af1278d880d20116d2e8ee35708861d0ed4e690
Michael Burke <mike.d.burke@gmail.com>
2016-06-09 23:53:03 -0400

    Get pascals triangle working with rough draft.

--- Pascals_Triangle/pascals_triangle.rb
+++ Pascals_Triangle/pascals_triangle.rb
@@ -1,13 +1,29 @@
+require 'pry'
 class Triangle
   def initialize(num_rows)
     @num_rows = num_rows
   end
 
   def rows
+    triangle = []
 
+    @num_rows.times do |index|
+      row = []
+      items_in_this_row = index + 1
+      items_in_this_row.times do |item|
+        if item == 0 || item == items_in_this_row - 1
+          row << 1
+        else
+          value = triangle[index - 1][item - 1] + triangle[index - 1][item]
+          row << value
+        end
+      end
+      triangle << row
+    end
+    triangle
   end
 end
 
-triangle = Triangle.new(2)
+triangle = Triangle.new(4)
 
 p triangle.rows
